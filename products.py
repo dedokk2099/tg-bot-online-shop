512e0279ba86eb97dd2d2176813d02ae9b573929
Yuri-Il <ilyushin_yuri@mail.ru>
2024-12-03 03:37:45 +0400

    Доработка функционала
    Работа с товарами по id. удаление товара, изменение в добавлении (выводится добавленный товар и даётся на выбор добавить ещё товар или обновить каталог), изменение в редактировании (выводится информация о редактируемом товаре и можно отменить редактирование до момента выбора атрибута)

--- products.py
+++ products.py
@@ -1,6 +1,6 @@
 products = [
-    {"name": "Банан", "price": 100, "description": "Банан из Африки, жёлтый", "quantity": 50, 'image': 'https://avatars.mds.yandex.net/i?id=45760c598fc7066e3b979e0574d1f5c504e023c6-10414509-images-thumbs&n=13'},
-    {"name": "Хлеб", "price": 200, "description": "Местный, свежий", "quantity": 20, 'image':  'https://cdn-img.perekrestok.ru/i/800x800-fit/xdelivery/files/5f/0c/6c03e02b315c21a6d1daca6bb029.jpg'},
-    {"name": "Телефон", "price": 300, "description": "Импортный, на Android", "quantity": 10, 'image':  'https://avatars.mds.yandex.net/i?id=5920a940a44bbefcb98868342436b832_l-4292261-images-thumbs&n=13'},
+    {'id': '1', "name": "Банан", "price": 100, "description": "Банан из Африки, жёлтый", "quantity": 50, 'image': 'https://avatars.mds.yandex.net/i?id=45760c598fc7066e3b979e0574d1f5c504e023c6-10414509-images-thumbs&n=13'},
+    {'id': '2', "name": "Хлеб", "price": 200, "description": "Местный, свежий", "quantity": 20, 'image':  'https://cdn-img.perekrestok.ru/i/800x800-fit/xdelivery/files/5f/0c/6c03e02b315c21a6d1daca6bb029.jpg'},
+    {'id': '3', "name": "Телефон", "price": 300, "description": "Импортный, на Android", "quantity": 10, 'image':  'https://avatars.mds.yandex.net/i?id=5920a940a44bbefcb98868342436b832_l-4292261-images-thumbs&n=13'},
     ]
     # Добавьте сюда больше товаров
