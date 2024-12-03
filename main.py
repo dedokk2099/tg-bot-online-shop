ad4f61a32cd3ca014af6db0614db8a69e5fc8266
Yuri-Il <ilyushin_yuri@mail.ru>
2024-12-03 21:02:46 +0400

    Команда /user, класс UserInterface и юзер-панель (заглушка)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import telebot
 from telebot import types
 import admin_interface
+import user_interface
 #from database import *
 
 bot = telebot.TeleBot('7344618019:AAEcLhHpsggJaSsVxfg9watitiIg08CR3zM')
 admin_interface_ = admin_interface.AdminInterface(bot)
+user_interface_ = user_interface.UserInterface(bot)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -21,6 +23,15 @@ def handle_admin(message):
     bot.send_message(message.chat.id, 'Админ-панель:', reply_markup=admin_interface_.generate_main_keyboard())
     print(f"Admin panel accessed by chat_id: {chat_id}, state reset.")
 
+@bot.message_handler(commands=['user'])
+def handle_user(message):
+    # Сбрасываем состояние пользователя
+    chat_id = message.chat.id
+    if chat_id in user_interface_.user_states:
+        user_interface_.user_states[chat_id] = {}
+    # Отображаем клавиатуру юзера
+    bot.send_message(message.chat.id, 'Юзер-панель:', reply_markup=user_interface_.generate_main_keyboard())
+    print(f"User panel accessed by chat_id: {chat_id}, state reset.")
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
---
+++ user_interface.py
@@ -0,0 +1,76 @@
+import telebot
+from telebot import types
+from PIL import Image
+import io
+import requests
+import os
+import uuid
+
+import products
+
+PHOTO_FOLDER = 'product_photos'
+
+
+class UserInterface:
+    def __init__(self, bot):
+        self.bot = bot
+        self.register_handlers()
+        self.products = products.products
+        self.user_states = {}
+
+    def setup_commands(self):
+        self.bot.set_my_commands([
+            telebot.BotCommand("/catalog", "Открыть каталог"),
+            telebot.BotCommand("/my_orders", "Посмотреть заказы"),
+            telebot.BotCommand("/cart", "Открыть корзину"),
+        ])
+
+    def generate_main_keyboard(self):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button_catalog = types.KeyboardButton("Товары")
+        button_my_orders = types.KeyboardButton("Мои заказы")
+        button_cart = types.KeyboardButton("Корзина")
+        markup.add(button_catalog, button_my_orders, button_cart)
+        return markup
+
+    def reset_user_state(self, chat_id):
+        if chat_id in self.user_states:
+            self.user_states[chat_id] = {}
+            print(f"State reset for chat_id: {chat_id}")
+
+    def handle_catalog(self, message):
+        self.bot.send_message(message.chat.id, "Список товаров в каталоге:")
+
+    def handle_my_orders(self, message):
+        self.bot.send_message(message.chat.id, "Список заказов:")
+
+    def handle_cart(self, message):
+        self.bot.send_message(message.chat.id, "Список товаров в корзине:")
+
+    def handle_unknown(self, message):
+        self.bot.send_message(message.chat.id, "Неизвестная команда")
+
+        
+    def register_handlers(self):
+        @self.bot.message_handler(commands=['catalog'])
+        def handle_catalog_command(message):
+            self.handle_catalog(message)
+
+        @self.bot.message_handler(commands=['my_orders'])
+        def handle_new_orders_command(message):
+            self.handle_my_orders(message)
+
+        @self.bot.message_handler(commands=['cart'])
+        def handle_in_progress_command(message):
+            self.handle_cart(message)
+
+        @self.bot.message_handler(func=lambda message: message.text in ["Товары", "Мои заказы", "Корзина"])
+        def handle_default(message):
+            if message.text == "Товары":
+                self.handle_catalog(message)
+            elif message.text == "Мои заказы":
+                self.handle_my_orders(message)
+            elif message.text == "Корзина":
+                self.handle_cart(message)
+            else:
+                self.handle_unknown(message)
