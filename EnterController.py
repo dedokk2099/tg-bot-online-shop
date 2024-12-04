3d89226bbd4bc3d570c1884d31cc43bc0995f43b
dedokk2099 <dedevdmc2099@gmail.com>
2024-12-05 00:00:23 +0400

    1. Добавлен role_switcher 2. В database добавлен класс для взаимодействия с данными пользователя 3. Упрощен main

--- EnterController.py
+++
@@ -1,6 +0,0 @@
-class EnterController:
-    def __init__():
-        super().__init__()
-
-    #def is_role():
-
--- database.py
+++ database.py
@@ -9,18 +9,50 @@ engine = create_engine('sqlite:///shop.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-class PhoneNumber(Base):
-    __tablename__ = 'phone_number'
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    number = Column(String, unique=False, nullable=False)
+    chat_id = Column(String, unique=True, nullable=False)
     role = Column(String, unique=False, nullable=False)
 
 Base.metadata.create_all(engine) 
 
+class User:
+    def __init__(self):
+        self.Base = Base
+
+    def _add_admin(self, chat_id):
+        user = session.query(Users).filter_by(chat_id=chat_id).first()
+        session.delete(user)
+        user = Users(chat_id=chat_id, role = 'Admin')
+        session.add(user)
+        session.commit()
+    
+    def add_new(self, chat_id):
+        user = Users(chat_id=chat_id, role = 'User')
+        session.add(user)
+        session.commit()
+
+    def is_new(self, chat_id):
+        user = session.query(Users).filter_by(chat_id=chat_id).first()
+        if user == None:
+            return True
+        else:
+            return False
+
+    def is_admin(self, chat_id):
+        user = session.query(Users).filter_by(chat_id=chat_id).first()
+        if user.role == 'Admin':
+            return True
+        else:
+            return False
+
+
 # Пример добавления
-# number = PhoneNumber(number='+79648589204', role = 'Admin')
-# session.add(number)
+#number = session.query(Users).filter_by(chat_id="2023435947").first()
+# session.delete(number)
 # session.commit()
 
-# item = session.query(PhoneNumber).filter_by(number="+79648589204").first()
-# print(item.number, item.role)
+# item = session.query(Users).filter_by(chat_id="2023435947").first()
+# print(item.chat_id, item.role)
+# print(item)
